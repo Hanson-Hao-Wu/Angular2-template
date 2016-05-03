e5c935a3179763d22e3bd9e8d0d3fba0c5f33e2f
Hao <hanson.hao.wu@gmail.com>
2016-05-02 22:50:46 -0400

    Initial commit

---
+++ dev/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular2 Template</h1>
+        <p>Hello World!</p>
+    `,
+})
+export class AppComponent {
+
+}
